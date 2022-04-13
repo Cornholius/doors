d599b6a3acb964b60f13b174ec5136e3a3ea6aa4
lyi <y.layshkin@gmail.com>
2022-04-13 17:32:18 +0300

    шаблон для коллекций

--- src/components/collections/Collections.jsx
+++ src/components/collections/Collections.jsx
@@ -1,25 +1,63 @@
 import React from 'react';
 import door from '../../images/about/door.png'
+import ekza from '../../images/collections/novella_logo_black.svg'
+
 
 const Collections = () => {
     return (
         <section className='collections'>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
-                <img className="about__pic" src={door} alt="about_door_pic"/>
+            
+            <div className="collections__item">
+                <div className="collections__title">
+                    <div className="collections__text">Коллекция</div>
+                    <img className="collections__tiitle__pic" src={ekza} alt="about_door_pic"/>
+                </div>
+                <div className="collections__doors">
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                </div>
+            </div>
 
+            <div className="collections__item">
+                <div className="collections__title">
+                    <div className="collections__text">Коллекция</div>
+                    <img className="collections__tiitle__pic" src={ekza} alt="about_door_pic"/>
+                </div>
+                <div className="collections__doors">
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                </div>
+            </div>
+
+            <div className="collections__item">
+                <div className="collections__title">
+                    <div className="collections__text">Коллекция</div>
+                    <img className="collections__tiitle__pic" src={ekza} alt="about_door_pic"/>
+                </div>
+                <div className="collections__doors">
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                </div>
+            </div>
+
+            <div className="collections__item">
+                <div className="collections__title">
+                    <div className="collections__text">Коллекция</div>
+                    <img className="collections__tiitle__pic" src={ekza} alt="about_door_pic"/>
+                </div>
+                <div className="collections__doors">
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                    <img className="collections__doors__pic" src={door} alt="about_door_pic"/>
+                </div>
+            </div>
         </section>
     );
 };
